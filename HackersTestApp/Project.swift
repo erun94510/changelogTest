55aaea149a9c5fdda01ecc7d4134ef5feadfbfba
배동훈 <erun9414@icloud.com>
2025-05-16 16:25:57 +0900

    feat(project): tuist 모듈 추가

---
+++ HackersTestApp/Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+let project = Project(
+    name: "HackersTestApp",
+    targets: [
+        .target(
+            name: "HackersTestApp",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.HackersTestApp",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["HackersTestApp/Sources/**"],
+            resources: ["HackersTestApp/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "HackersTestAppTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.HackersTestAppTests",
+            infoPlist: .default,
+            sources: ["HackersTestApp/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "HackersTestApp")]
+        ),
+    ]
+)
