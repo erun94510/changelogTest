b13ff00bb402f9e4b92afcd67ffa023cad99716f
배동훈 <erun9414@icloud.com>
2025-05-16 16:38:11 +0900

    feat: ScrollView로 데이터 뷰 구현

--- HackersTestApp/HackersTestApp/Sources/ContentView.swift
+++ HackersTestApp/HackersTestApp/Sources/ContentView.swift
@@ -1,17 +1,20 @@
 import SwiftUI
 
 public struct ContentView: View {
-    public init() {}
-
-    public var body: some View {
-        Text("Hello, World!")
-            .padding()
-    }
+	public init() {}
+	
+	public var body: some View {
+		ScrollView {
+			ForEach((0...15), id: \.self) { index in
+				Text("\(index)")
+			}
+		}
+	}
 }
 
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
